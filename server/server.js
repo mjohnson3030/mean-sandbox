ddc72e96387adb41413a071571e5562059ad57b4
Matt Johnson <msjohnson3030@gmail.com>
2020-07-31 19:54:23 -0400

    Including public resources

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 var connect = require('connect');
 var express = require('express');
+var path = require('path');
 var app = express();
 //var mongoose = require('mongoose');
 var dbConfig = require('./config/database');
 
 //mongoose.connect(dbConfig.url);
 
-app.use(connect.static(__dirname + '../client'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'img')));
 app.use(connect.logger('dev'));
 app.use(connect.json());
 app.use(connect.urlencoded());
